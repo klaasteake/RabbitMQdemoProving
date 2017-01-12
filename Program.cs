d66416755a44f59faf2ec72c1fd912b60d585787
Klaas-Teake Slager <Klaas-Teake Slager>
2017-01-12 21:27:41 +0100

    Initial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using rabbitmq_demo;
+using RabbitMQ.Client;
+using Autofac;
+using System.Threading;
+
+
+namespace RabbitMQdemoProving
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RabbitMQdemoProving")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9d93a23-9960-4e74-b498-9f8b048482ab")]
